1453963ed41bea66d83ffd2c4d5d080c501a2a0c
prnth <pranith_h@yahoo.com.sg>
2025-10-08 05:00:13 +0530

    ios fullscreen

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -19,16 +19,13 @@ function App() {
 }
 
 const IframePositionWrapper = ({ children }: { children: React.ReactNode }) => {
-  const [isInsideIframe, setIsInsideIframe] = useState(false);
-
-  useEffect(() => {
-    setIsInsideIframe(window.self !== window.top);
-  }, []);
+  const isInsideIframe = window && (window.self !== window.top);
+  const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream;
 
   return isInsideIframe ? <div className='z-50'>{children}</div> :
-    <div className='fixed w-screen h-screen pointer-events-none select-none z-50'>
+    <div className={`fixed w-screen ${isIOS ? 'h-[calc(100%+env(safe-area-inset-top))]' : 'h-screen'} pointer-events-none select-none z-50`}>
       <SkyShader />
-      <div className={`bottom-2 left-1/2 -translate-x-1/2 absolute transition-all pointer-events-auto flex flex-col`}>
+      <div className={`${isIOS ? 'bottom-8' : 'bottom-2'} left-1/2 -translate-x-1/2 absolute transition-all pointer-events-auto flex flex-col`}>
         {children}
       </div>
     </div>
