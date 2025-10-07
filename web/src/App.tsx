4351c6b733aa07fdf23d1292e46d2a7a7fa06bdd
prnth <pranith_h@yahoo.com.sg>
2025-10-08 05:13:21 +0530

    err

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -22,9 +22,9 @@ const IframePositionWrapper = ({ children }: { children: React.ReactNode }) => {
   const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream;
 
   return isInsideIframe ? <div className='z-50'>{children}</div> :
-    <div className={`fixed w-screen ${isIOS ? 'h-[calc(100%+env(safe-area-inset-top))]' : 'h-screen'} pointer-events-none select-none z-50`}>
+    <div className={`fixed w-screen ${isIOS ? '-mt-[4rem] h-[calc(100%+env(safe-area-inset-top))]' : 'h-screen'} pointer-events-none select-none z-50`}>
       <SkyShader />
-      <div className={`${isIOS ? 'bottom-8' : 'bottom-2'} left-1/2 -translate-x-1/2 absolute transition-all pointer-events-auto flex flex-col`}>
+      <div className={`${isIOS ? 'bottom-[calc(env(safe-area-inset-bottom)+4rem)]' : 'bottom-2'} left-1/2 -translate-x-1/2 absolute transition-all pointer-events-auto flex flex-col`}>
         {children}
       </div>
     </div>
