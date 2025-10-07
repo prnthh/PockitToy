61d7f299baf5efefc7eaa2377ec127e2f2775861
prnth <pranith_h@yahoo.com.sg>
2025-10-07 22:57:40 +0530

    scroll

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,6 +6,7 @@ import SaveBlobProvider from './shared/SaveBlobProvider'
 function App() {
   return (
     <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
       <AudioProvider>
         <SaveBlobProvider>
           <PockitToy roomId='my-room-id' />
